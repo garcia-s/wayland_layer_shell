7b97779e0d658101a4826508a3f21974ec1c656d
Juan García <43949658+garcia-s@users.noreply.github.com>
2024-08-27 06:53:48 -0500

    Getting a layer correctly, still not working properly but...

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -11,15 +11,20 @@ import 'package:wayland_layer_shell_example/set_layer.dart';
 import 'package:wayland_layer_shell_example/set_margins.dart';
 
 Future<void> main() async {
+  print("Does this work");
   WidgetsFlutterBinding.ensureInitialized();
   final waylandLayerShellPlugin = WaylandLayerShell();
-  bool isSupported = await waylandLayerShellPlugin.initialize(650, 600);
+  bool isSupported = await waylandLayerShellPlugin.initialize(0, 80);
   if (!isSupported) {
-    runApp(const MaterialApp(home: Center(child: Text('Not supported'))));
+    runApp(
+      const MaterialApp(
+        home: Center(
+          child: Text('Not supported'),
+        ),
+      ),
+    );
     return;
   }
-  await waylandLayerShellPlugin
-      .setKeyboardMode(ShellKeyboardMode.keyboardModeExclusive);
   runApp(const MyApp());
 }
 
@@ -33,50 +38,8 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Wayland Layer Shell example'),
-        ),
-        body: const Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SetMonitor(),
-                  SizedBox(width: 40),
-                  SetExclusiveZone(),
-                ],
-              ),
-              SizedBox(height: 20),
-              SetLayer(),
-              SizedBox(height: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SetKeyboard(),
-                  SizedBox(
-                    width: 120,
-                    child: TextField(
-                      autofocus: true,
-                      decoration:
-                          InputDecoration(labelText: 'Test Keyboard Mode'),
-                    ),
-                  )
-                ],
-              ),
-              SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [SetAnchors(), SizedBox(width: 40), SetMargins()],
-              ),
-            ],
-          ),
-        ),
-      ),
+    return Container(
+      color: Colors.green,
     );
   }
 }
